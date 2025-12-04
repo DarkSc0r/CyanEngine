41bb20ba0742419a9e64ba61be6200185e96709e
DarkSc0r <DarkSc0r500@gmail.com>
2025-12-03 23:00:59 -0500

    Fixed game loop function

--- src/main/GameRendering.java
+++ src/main/GameRendering.java
@@ -2,10 +2,10 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
-
+import java.lang.Thread;
 import javax.swing.JPanel;
 
-public class GameRendering extends JPanel{
+public class GameRendering extends JPanel implements Runnable{
     // SCREEN SETTINGS
     final int tile_size = 16; // 16x16 tile size || Default tile size
     final int scale = 3;
@@ -16,9 +16,24 @@ public class GameRendering extends JPanel{
     final int screen_width = updated_tile_size * max_screen_col;
     final int screen_height = updated_tile_size * max_screen_row;
 
+    Thread game_thread;
+
     public GameRendering() {
         this.setPreferredSize(new Dimension(screen_width, screen_height));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+    }
+
+    public void start_game_thread() {
+        game_thread = new Thread(this);
+
+        game_thread.start();
+    }
+
+    @Override
+    public void run() {
+        
+
+
     }
 }
